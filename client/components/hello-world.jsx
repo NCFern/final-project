139370cd690a7112009e223ec48b1da0062184fa
Nicholas Fernandez <Nickfer610@gmail.com>
2022-09-21 20:34:48 +0000

    form looking better

--- client/components/hello-world.jsx
+++ client/components/hello-world.jsx
@@ -2,76 +2,95 @@ import React from 'react';
 
 export default function HelloWorld(props) {
   return (
-    // <h1>Hello, World!!!</h1>
     <div>
       <header className='navi-bar'>
         <div className='container'>
           Ledger
         </div>
       </header>
+
       <div className='container'>
         <div data-view='nurse-form'>
           <form id='new-form'>
-              <div className='row'>
-                <div className='column-full text-center'>
-                  <h1>Nurse Entry</h1>
+            <div className='row'>
+              <div className='column-full text-center heading-margin'>
+                <h1>Nurse Entry</h1>
+              </div>
+            </div>
+
+            <div className='row'>
+
+              <div className='column-half'>
+                {/* placeholder pic here */}
+                <img src='../images/placeholder.jpeg' className='img-fluid'></img>
+                <div className='custom-file'>
+                  <label className='file-input-label' htmlFor='nurseFile'></label>
+                  <input required type='file' className='file-input' id='nurseFile'></input>
                 </div>
               </div>
-              <div className='row'>
-                <div className='column-half text-center'>
-                  <h1>test</h1>
+
+              <div className='column-half'>
+                <div className='form-row'>
+                  <div className='form-group col-md-6'>
+                    <label htmlFor='firstName'>First Name</label>
+                    <input required type='firstName' className='form-control' id='inputFirstName'></input>
+                  </div>
+                  <div className='form-group col-md-6'>
+                    <label htmlFor='lastName'>Last Name</label>
+                    <input required type='lastName' className='form-control' id='inputLastName'></input>
+                  </div>
                 </div>
 
-                <div className='column-half text-center'>
-                  <div className='row'>
-                    <div className='column-half'>
-                      <label className>First Name</label>
-                    </div>
-                    <div className='column-half'>
-                      <label>Last Name</label>
-                    </div>
-                  </div>
-                  <div className='row'>
-                    <div className='column-half'>
-                      <input required name='first-name' type='text' className='input-text'></input>
-                    </div>
-                    <div className='column-half'>
-                      <input required name='last-name' type='text' className='input-text'></input>
-                    </div>
-                  </div>
-                  <div className='row'>
-                    <label>Address</label>
-                  </div>
-                  <div className='row'>
-                    <input required name='address' type='text' className='input-text'></input>
-                  </div>
-                <div className='row'>
-                  <label>Phone Number</label>
+                <div className='form-group'>
+                  <label htmlFor='inputAddress'>Address</label>
+                  <input required type='text' className='form-control' id='inputAddress'></input>
                 </div>
-                <div className='row'>
-                  <input required name='phone-number' type='number' className='input-text'></input>
-                </div>
-                <div className='row'>
-                  <label>Birthday</label>
-                </div>
-                <div className='row'>
-                  <input required name='birthday' type='number' className='input-text'></input>
+                <div className='form-group'>
+                  <label htmlFor='inputAddress2'>Address 2</label>
+                  <input required type='text' className='form-control' id='inputAddress2'></input>
                 </div>
 
-                <div className="row">
-                  <div className="column-full">
-                    <div className="form-actions text-center">
-                      <button type="submit" className="submit-button">SUBMIT</button>
-                    </div>
+                <div className='form-row'>
+                  <div className='form-group col-md-6'>
+                    <label htmlFor='inputCity'>City</label>
+                    <input required type='text' className='form-control' id='inputCity'></input>
+                  </div>
+                  <div className='form-group col-md-4'>
+                    <label htmlFor='inputState'>State</label>
+                    <select id='inputState' className='form-control'>
+                      <option required disabled>Choose...</option>
+                      <option>California</option>
+                      <option>Nevada</option>
+                      <option>Arizona</option>
+                      <option>Oregon</option>
+                      <option>other</option>
+                    </select>
+                  </div>
+                  <div className='form-group col-md-2'>
+                    <label htmlFor='inputZip'>Zip</label>
+                    <input required type='text' className='form-control' id='inputAddress'></input>
                   </div>
                 </div>
+
+                <div className='form-group'>
+                  <label htmlFor='inputPhone'>Phone Number</label>
+                  <input required type='text' className='form-control' id='inputPhone'></input>
                 </div>
 
+                <div className='form-group'>
+                  <label htmlFor='inputBirthday'>Birthday</label>
+                  <input required type='text' className='form-control' id='inputBirthday'></input>
+                </div>
               </div>
+            </div>
+            <div className='text-right'>
+                <button type="submit" className="btn btn-primary">Sign in</button>
+            </div>
+
           </form>
         </div>
-
       </div>
+
     </div>
 
   );
